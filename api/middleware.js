e63d2b96f62031e0ab865ddc0102dd51174c9292
joestardodoA <joestardodo@gmail.com>
2026-02-06 21:45:36 +0800

    Delete api/middleware.js

--- api/middleware.js
+++
@@ -1,9 +0,0 @@
-export default async function (request) {
-  const url = new URL(request.url);
-
-  // 🎯 直接把所有请求的目标改成 Google
-  url.hostname = 'generativelanguage.googleapis.com';
-
-  // 🚀 转发！
-  return fetch(url, request);
-}
